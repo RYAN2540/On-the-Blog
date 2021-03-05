9a752b26867857ecb5175a84ae473fda02d7c7ea
Your Name <you@example.com>
2021-03-05 13:49:12 +0300

    create main blueprint and define 404 error handling

---
+++ config.py
@@ -0,0 +1,31 @@
+import os
+
+class Config:    
+    SECRET_KEY = os.environ.get('SECRET_KEY')    
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 465
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL=True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL_TEST")
+
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL_DEV")
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+'test':TestConfig
+}
